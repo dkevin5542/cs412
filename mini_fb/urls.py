d4622d980d53237f6d6a6f2bad8d6e19ac59c720
dkevin5542 <dkevin@bu.edu>
2024-10-04 15:30:19 -0400

    fixed the url

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -3,7 +3,9 @@ from django.conf import settings
 from . import views
 
 urlpatterns = [
-    path(r'', views.base, name='base'),
+    # path(r'', views.base, name='base'),
+    path(r'', views.ShowAllView.as_view(), name='show_all'),
+
     path('show_all', views.ShowAllView.as_view(), name='show_all_profile'),
     
     
